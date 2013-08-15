ad989e16539481928a5f8d6ac32d9f01e2307920
Jordan Leigh <AlwaysBCoding@gmail.com>
2013-08-14 23:13:46 -0400

    Adds special skills

--- app/models/expert.rb
+++ app/models/expert.rb
@@ -2,6 +2,7 @@ class Expert < User
   has_many :appointments, foreign_key: "expert_id"
   has_many :taggings
   has_many :tags, through: :taggings
+  has_many :special_skills
 
   validates :hourly_rate, presence: true, numericality: true
 

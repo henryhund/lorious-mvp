73111c7dbfcbdec8e19a75befc6cb571d6c086e4
Jordan Leigh <AlwaysBCoding@gmail.com>
2013-08-21 12:30:46 -0500

    form and routes to send messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,8 +7,8 @@ class MessagesController < ApplicationController
   end
 
   def create
-    appointment = params[:appointment_id]
-    message = Message.new appointment_id: appointment, from_user: current_user, body: params[:body]
+    appointment, to_user = params[:appointment_id], User.find_by_username(params[:to_user_username])
+    message = Message.new appointment_id: appointment, from_user: current_user, to_user: to_user, subject: params[:subject], body: params[:body]
     if message.save!
       flash[:notice] = t('message.on_appointment.create.success')
       if appointment.present?
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130815030756) do
+ActiveRecord::Schema.define(version: 20130821172047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 20130815030756) do
     t.boolean  "viewed",          default: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "subject"
   end
 
   create_table "special_skills", force: true do |t|
